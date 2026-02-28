4349e4c405f115e46786b60a031509201c689f39
Skullfurious <34455502+Skullfurious@users.noreply.github.com>
2026-02-28 16:41:07 -0400

    Implement external functions from scanner.c

--- grammar.js
+++ grammar.js
@@ -9,7 +9,30 @@
 
 export default grammar({
   name: "denizenscript",
+  // Copied from https://github.com/tree-sitter/tree-sitter-python/blob/master/grammar.js implements their scanner.c file.
+  externals: $ => [
+    $._newline,
+    $._indent,
+    $._dedent,
+    $.string_start,
+    $._string_content,
+    $.escape_interpolation,
+    $.string_end,
 
+    // Mark comments as external tokens so that the external scanner is always
+    // invoked, even if no external token is expected. This allows for better
+    // error recovery, because the external scanner can maintain the overall
+    // structure by returning dedent tokens whenever a dedent occurs, even
+    // if no dedent is expected.
+    $.comment,
+
+    // Allow the external scanner to check for the validity of closing brackets
+    // so that it can avoid returning dedent tokens between brackets.
+    ']',
+    ')',
+    '}',
+    'except',
+  ],
   rules: {
     // TODO: add the actual grammar rules
     source_file: $ => "hello"
